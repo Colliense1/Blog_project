ffde0f214452d7f716252e64317af96037fdabfc
Colliense Podder Prodip <colliensepodder25@gmail.com>
2024-10-29 23:43:30 +0600

    search by title or category

--- blog_project/views.py
+++ blog_project/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from posts.models import Post
+from django.db.models import Q
 
 def home(request):
     query = request.GET.get('q') 
-    if query:
-        data = Post.objects.filter(title__icontains=query)  
-    else:
-        data = Post.objects.all()  
 
-    return render(request, 'home.html', {'data': data})
+    data = Post.objects.all()
+    if query:
+       
+        data = data.filter(Q(title__icontains=query) | Q(category__name__icontains=query))  # Adjust according to your model
+
+    return render(request, 'home.html', {'data': data, 'query': query})
